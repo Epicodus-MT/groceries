8a169285faf6f7edb2b3b5c6eb3989319f153657
Mara Timberlake and Steve Zaske <mara@>
2017-06-05 14:51:38 -0700

    initial html and js code files

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(function(){
+  //this code runs when page is ready
+
+  //this creates eventlisterner for the addItem button
+  $('#addItem').click(function(event){
+    event.preventDefault(); //this stops the form from submitting
+
+    //hide the form
+    //display full list of items
+    //
+  });
+
+  //this creates an event listerner for the submit button
+  $('#submit').click(function(event){
+    event.preventDefault(); //this stops the form from submitting
+
+  });
+
+});
