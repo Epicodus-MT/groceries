e0e0673487e74f39c2798e8b2fc964df235d871b
Mara Timberlake and Steve Zaske <steve@>
2017-06-05 15:43:01 -0700

    add looping array logic

--- js/script.js
+++ js/script.js
@@ -1,19 +1,44 @@
 $(function(){
   //this code runs when page is ready
 
+  //basic logic needed for our page
+  //create an empty array
+  var groceryList = [];
+
   //this creates eventlisterner for the addItem button
   $('#addItem').click(function(event){
     event.preventDefault(); //this stops the form from submitting
 
-    //hide the form
-    //display full list of items
-    //
+    //this needs append the input text to the array
+    groceryList.push($("#textField").val());
+    //clear the textField value
+
+    $("#textField").val("");
+
+    console.log(groceryList);
   });
+
+
 
   //this creates an event listerner for the submit button
   $('#submit').click(function(event){
     event.preventDefault(); //this stops the form from submitting
 
+    //hide the form
+    $("#form").hide();
+    //another way to hide an HTML item
+    // $("#form").addClass("hidden");
+
+    //loop through the array
+    groceryList.forEach(function(item){
+      //in each loop, append a line item in results
+      $("#results").append("<li>" + item + "</li>");
+    });
+
+
+
+    //display full list of items
+    console.log("hidden");
   });
 
 });
